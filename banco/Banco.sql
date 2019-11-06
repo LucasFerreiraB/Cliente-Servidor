8da67736bbec06f09662dfcd96f6c2bf4c20ae36
LucasFerreiraB <47697686+LucasFerreiraB@users.noreply.github.com>
2019-11-06 20:14:09 -0300

    Atualizacao do banco

--- banco/Banco.sql
+++ banco/Banco.sql
@@ -1,49 +1,47 @@
-drop database if exists cursos;
-create database cursos
+drop database if exists Curso;
+create database Curso
 default collate utf8_general_ci
 default character set utf8;
 
-use cursos;
+use Curso;
 
 drop table if exists Usuario;
 create table if not exists Usuario(
-	IdUsuario bigint unsigned not null primary key auto_increment,
-	Nome varchar(50) not null,
-	Email varchar(60) not null,
-	Senha varchar(32) not null,
-	hashtoken varchar(32),
-	Timetoken timestamp
+	idUsuario int unsigned not null primary key auto_increment,
+	nome varchar(50) not null,
+	email varchar(60) not null,
+	senha varchar(32) not null,
+	hashToken varchar(32),
+	timeToken timestamp
 ) default charset = utf8 engine = InnoDB;
   
-insert into Usuario (Nome, Email, Senha) values 
+insert into Usuario (nome, email, senha) values 
 ('Lucas', 'lucasferreira9b@gmail.com', MD5(5163131)); 
 
 drop table if exists Cursos;
 create table if not exists Cursos(
-	idCurso bigint unsigned not null auto_increment primary key,
-	Sobre text,
-	CargaHoraria timestamp not null
+	idCurso int unsigned not null auto_increment primary key,
+	sobre text,
+	cargaHoraria timestamp not null
 ) default charset = utf8 engine = InnoDB;
 
-insert into Cursos (idCurso, Sobre, CargaHoraria) values
+insert into Cursos (idCurso, sobre, cargaHoraria) values
 (1, 'asdasdasdasd', '2019-09-24');
 
 drop table if exists Videos;        
 create table if not exists Videos(
-	idVideo bigint not null primary key,
-	curso bigint unsigned not null,
-	Tempo time not null,
-	Tema varchar (15) NOT null,
-    	constraint FK_CURSO_CURSOS_IDCURSO foreign key(curso) references Cursos(idCurso)
+	idVideo int not null primary key,
+	idCurso int unsigned not null,
+	tempo time not null,
+	tema varchar (15) not null,
+    	constraint FK_CURSO_CURSOS_IDCURSO foreign key(idCurso) references Cursos(idCurso)
 ) default charset = utf8 engine = InnoDB;
 
-drop table if exits Usuario_Curso;
-create table if not exists Usuario_Curso(
-	idUS integer (8) null primary key,
-	idCurso integer (8) null,
-    	idUsuario integer (8) null,
+drop table if exits UsuarioCursa;
+create table if not exists UsuarioCursa(
+	idUS int unsigned not null auto_increment primary key,
+	idCurso int unsigned not null,
+    	idUsuario int unsigned not null,
 	constraint FK_IDCURSO_CURSO_IDCURSO foreign key (idCurso) references Curso (idCurso),
-	contraint FK_IDCURSO_USUARIO_IDUSUARIO foreign key (idUsuario) references Usuario (idUsuario)
+	constraint FK_IDCURSO_USUARIO_IDUSUARIO foreign key (idUsuario) references Usuario (idUsuario)
 ) default charset = utf8 Engine = InnoDB ;
-    
-
